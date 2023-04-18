94a1c67b866f16faf55cec8de680803678feda77
saluumaa <salumahassan02@gmail.com>
2023-04-18 15:16:51 +0300

    Add test for rocketsSlice file

---
+++ src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <div>
+    <header className="header">
+      <nav className="nav-bar">
+        <div className="logo">
+          Space Travelers Hub
+        </div>
+        <ul>
+          <li><NavLink to="/">RocketsList</NavLink></li>
+          <li><NavLink to="/MyProfile">My Profile</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+);
+
+export default Navbar;
